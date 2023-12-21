945f93b9214e0096f7c2d8ef39632b87f9b22cbb
Vanshika432 <vanshikarathore202@gmail.com>
2023-12-21 22:39:56 +0530

    done

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
 require("dotenv").config();
 
-const app = express();
 app.use(cors());
 app.use(express.json());
 
@@ -19,11 +22,30 @@ mongoose
     console.log(err.message);
   });
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-const server = app.listen(process.env.PORT,()=>{
-    console.log(`server is listening on ${process.env.PORT}`)
-})
- 
-app.get('/',(req,res) => {
-    res.send("Hello");
-})
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
